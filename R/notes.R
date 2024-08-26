e888909caeab4ee31b0049944a40adc6111a1418
Fhui28 <fhui28@gmail.com>
2024-08-27 05:35:03 +1000

    removing nb2, tweedielogfam and betalogitfam to use the corresponding family functions in sdmTMB instead.
    Also moving notes around

--- R/notes.R
+++ R/notes.R
@@ -1,12 +1,9 @@
-#' **TODO**
-#' - Speed up glmmTMB [why does initial values not make much of a difference? Probably because of overhead on glmmTMB, but hard to change this givne things like mgcv::gam will be slower in the actual fitting process...]
-#' 
 #' 
 #' 
 #' Used in assam.R
 function() {
     formula = paste("~ ", paste0(colnames(covariate_dat), collapse = "+")) %>% as.formula
-    family <- nb2()
+    family <- nbinom2()
     y = simdat$y
     data = covariate_dat
     offset = NULL
@@ -17,7 +14,23 @@ function() {
     uncertainty_quantification <- TRUE
     control = list(max_iter = 500, tol = 1e-5, temper_prob = 0.8, trace = TRUE)
     bootstrap_control = list(num_boot = 100, ci_alpha = 0.05, seed = NULL, ci_type = "percentile")
-}
+    }
+
+#'
+#'
+#' Used in .quadapprox_fm.R
+function() {
+    formula = paste("~ ", paste0(colnames(covariate_dat), collapse = "+")) %>% as.formula
+    family <- nbinom2()
+    resp = simdat$y
+    data = covariate_dat
+    offset = NULL
+    trial_size = 1
+    do_parallel = TRUE
+    num_nuisance_perspp = NULL
+    start_archetype_labels = NULL
+    start = NULL
+    }
 
 
 #'
@@ -33,18 +46,4 @@ function() {
     coverage = 0.95
 }
 
-
-#'
-#'
-#' Used in .quadapprox_fm.R
-
-function() {
-    formula = paste("~ ", paste0(colnames(covariate_dat), collapse = "+")) %>% as.formula
-    family <- tweedielogfam()
-    resp = simdat$y
-    data = covariate_dat
-    offset = NULL
-    trial_size = 1
-    do_parallel = TRUE
-}
 
