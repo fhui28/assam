6d2980ce45e9bed18314a4dc90f0aa961cbaa34d
Fhui28 <fhui28@gmail.com>
2024-09-12 16:09:53 +1000

    Adding some a note script for testing sdmTMB in parallel.
    Ignored. Was posted as an issue on sdmTMB

--- R/notes.R
+++ R/notes.R
@@ -1,38 +1,10 @@
-#' General testing
-function(){
-    library(sdmTMB)
-    library(glmmTMB)
-    library(mgcv)
-    
-    sdmfit <- sdmTMB(formula = paste("response ~ ", paste0("s(", colnames(covariate_dat)[1:3], ")", collapse = "+"), "+", paste0(colnames(covariate_dat)[4:7], collapse = "+")) %>% as.formula,
-                     data = data.frame(response = simdat$y[,1], covariate_dat),
-                     family = nbinom2(),
-                     spatial = FALSE)
-    
-    summary(sdmfit)
-    sdmfit_covariance <- sdmfit$sd_report$jointPrecision %>% solve
-
-    
-    gamfit <- gam(formula = paste("response ~ ", paste0("s(", colnames(covariate_dat)[1:3], ")", collapse = "+"), "+", paste0(colnames(covariate_dat)[4:7], collapse = "+")) %>% as.formula,
-                 data = data.frame(response = simdat$y[,1], covariate_dat),
-                 family = nb(),
-                 method = "ML")
-    
-    summary(gamfit)
-    summary(sdmfit)
-    vcov(gamfit) %>% diag %>% sqrt
-    sdmfit_covariance %>% diag %>% sqrt #' Need to rearrange such that all the smoothers are put together?
-    
-    }
-
-
 #' Used in assam.R
 function() {
-    formula = paste("~ ", paste0(colnames(covariate_dat), collapse = "+")) %>% as.formula
-    family <- tweedie()
+    formula = paste("~ ", paste0(colnames(covariate_dat)[-c(1:2)], collapse = "+")) %>% as.formula
+    family <- nbinom2()
     y = simdat$y
     data = covariate_dat
-    mesh = NULL #sdmTMB::make_mesh(covariate_dat, xy_cols = c("x", "y"), n_knots = 50)
+    mesh = sdmTMB::make_mesh(covariate_dat, xy_cols = c("x", "y"), n_knots = 50)
     offset = NULL
     trial_size = 1
     do_parallel = TRUE
@@ -48,10 +20,10 @@ function() {
 #' Used in .quadapprox_fn.R
 function() {
     formula = paste("~ ", paste0(colnames(covariate_dat)[-(1:2)], collapse = "+")) %>% as.formula
-    family <- family
+    family <- nbinom2()
     resp = simdat$y
     data = covariate_dat
-    mesh = mesh
+    mesh = sdmTMB::make_mesh(covariate_dat, xy_cols = c("x", "y"), n_knots = 50)
     offset = NULL
     trial_size = 1
     do_parallel = TRUE
@@ -59,21 +31,19 @@ function() {
     start_archetype_labels = NULL
     start = NULL
     
-    
-    l = 1
     tmp_formula <- as.formula(paste("response", paste(as.character(formula), collapse = " ") ) )
-    fit0_orig <- sdmTMB(tmp_formula,
-                   data = data.frame(response = resp[,l], data, trial_size = trial_size), 
-                   spatial =  TRUE,
-                   mesh = mesh,
-                   family = family)
-    
+    fit0_orig <- sdmTMB::sdmTMB(tmp_formula,
+                            data = data.frame(response = simdat$y[,l], covariate_dat, trial_size = trial_size), 
+                            spatial = TRUE,
+                            mesh = mesh,
+                            family = nbinom2())
+
 
     use_pars <- fit0_orig$tmb_obj$env$parList()
-    use_pars[["b_j"]] <- c(do_em$new_spp_intercept[l], do_em$new_betas[which.max(do_em$post_prob[l,]),])
-    use_pars[["ln_phi"]] <- do_em$new_nuisance[l, grep("ln_phi", colnames(do_em$new_nuisance))]
-    use_pars[["ln_tau_O"]] <- do_em$new_nuisance[l, grep("ln_tau_O", colnames(do_em$new_nuisance))]
-    use_pars[["ln_kappa"]] <- matrix(do_em$new_nuisance[l, grep("ln_kappa", colnames(do_em$new_nuisance))], 2, 1)
+    use_pars[["b_j"]] <- rnorm(use_pars[["b_j"]]) #c(do_em$new_spp_intercept[l], do_em$new_betas[which.max(do_em$post_prob[l,]),])
+    #use_pars[["ln_phi"]] <- do_em$new_nuisance[l, grep("ln_phi", colnames(do_em$new_nuisance))]
+    #use_pars[["ln_tau_O"]] <- do_em$new_nuisance[l, grep("ln_tau_O", colnames(do_em$new_nuisance))]
+    #use_pars[["ln_kappa"]] <- matrix(do_em$new_nuisance[l, grep("ln_kappa", colnames(do_em$new_nuisance))], 2, 1)
     new_tmbobj <- TMB::MakeADFun(
         data = fit0_orig$tmb_data,
         profile = fit0_orig$control$profile,
@@ -83,28 +53,31 @@ function() {
         DLL = "sdmTMB",
         silent = TRUE
         )
-    new_fit0 <- nlminb(start = new_tmbobj$par, 
-                       objective = new_tmbobj$fn, 
-                       gradient = new_tmbobj$gr)
-
+    new_fit0 <- nlminb(start = new_tmbobj$par,
+                       objective = new_tmbobj$fn,
+                       gradient = new_tmbobj$gr,
+                       control = fit0_orig$nlminb_control)
+    r <- new_tmbobj$report(new_tmbobj$env$last.par.best)
+    cbind(predict(fit0_orig)$omega_s, r$omega_s_A)
+    
+    
     new_fit0 <- sdmTMB(tmp_formula,
-                        data = data.frame(response = resp[,l], data, trial_size = trial_size), 
-                        spatial =  TRUE,
+                       data = data.frame(response = simdat$y[,l], covariate_dat, trial_size = trial_size), 
+                       spatial =  TRUE,
                         mesh = mesh,
-                        control = sdmTMB::sdmTMBcontrol(
+                        control = sdmTMBcontrol(
                             start = list(
-                                b_j = c(do_em$new_spp_intercept[l], do_em$new_betas[which.max(do_em$post_prob[l,]),]),
-                                ln_phi = do_em$new_nuisance[l, grep("ln_phi", colnames(do_em$new_nuisance))],
-                                ln_tau_O = do_em$new_nuisance[l, grep("ln_tau_O", colnames(do_em$new_nuisance))],
-                                ln_kappa = matrix(do_em$new_nuisance[l, grep("ln_kappa", colnames(do_em$new_nuisance))], 2, 1)),
-                            map = list(b_j = as.factor(rep(NA, 1+num_X)), ln_phi = as.factor(NA), ln_tau_O = as.factor(NA), ln_kappa = as.factor(matrix(NA, 2, 1)))
+                                 b_j = as.vector(c(do_em$new_spp_intercept[l], do_em$new_betas[which.max(do_em$post_prob[l,]),])),
+                                 ln_phi = 1, #do_em$new_nuisance[l, grep("ln_phi", colnames(do_em$new_nuisance))],
+                                 ln_tau_O = 0.5, #do_em$new_nuisance[l, grep("ln_tau_O", colnames(do_em$new_nuisance))],
+                                 ln_kappa = matrix(1, 2, 1)), #do_em$new_nuisance[l, grep("ln_kappa", colnames(do_em$new_nuisance))]
+                            map = list(ln_phi = as.factor(NA), ln_tau_O = as.factor(NA), ln_kappa = as.factor(matrix(NA, 2, 1)), b_j = as.factor(rep(NA, 1+num_X))),
+                            lower = list(b_j = as.vector(c(do_em$new_spp_intercept[l], do_em$new_betas[which.max(do_em$post_prob[l,]),]) - 1e-1)),
+                            upper = list(b_j = as.vector(c(do_em$new_spp_intercept[l], do_em$new_betas[which.max(do_em$post_prob[l,]),]) + 1e-1)),
                             ),
-                       do_fit = TRUE,
-                        family = family)
-
-
-    }
-
+                       family = family)
+}
+    
 #'
 #'
 #' Used in predict.assam.R
