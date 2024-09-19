152db77e181bd716bc85117b1edd30801b56665b
fhui28 <fhui28@gmail.com>
2024-09-19 20:46:29 +1000

    Adding capacity of predict to predict for asSAMs with spp-specific spatial fields included.
    Uncertainty quantification yet to be done

--- R/predict.assam.R
+++ R/predict.assam.R
@@ -12,8 +12,6 @@
 #' If \code{type = "archetype"}, then archetypal predictions are constructed on the scale of the linear predictors i.e., \eqn{x_i^\top\beta_k};
 #' If \code{type = "species_max"}, then species-specific predictions on the scale of the responses i.e., \eqn{\mu_{ij}}, are constructed based on the most likely archetype the species belongs to (as based on the posterior probabilities);
 #' If \code{type = "species_mean"}, then species-specific predictions on the scale of the responses i.e., \eqn{\mu_{ij}}, are constructed based on a weighted mean of the predictions from each archetype, where the weights are given by the posterior probabilities.
-#' 
-#' Note for both types of species-specific predictions, **currently uncertainty of the posterior probabilities are not taken into account...this is on the to-do list!**
 #' @param se_fit When this is set to \code{TRUE} (not default), then uncertainty intervals are returned for the point predictions.
 #' @param num_cores To speed up calculation of the uncertainty intervals, parallelization can be performed, in which case this argument can be used to supply the number of cores to use in the parallelization. Defaults to \code{detectCores()-2}.
 #' @param coverage The coverage probability of the uncertainty intervals for prediction. Defaults to 0.95, which corresponds to 95% uncertainty intervals.
@@ -103,8 +101,8 @@
 #' @importFrom sdmTMB sdmTMB
 #' @importFrom parallel detectCores
 #' @importFrom stats as.formula model.matrix
+#' @importFrom TMB MakeADFun sdreport
 #' @md
-
 
     
 predict.assam <- function(object, 
@@ -120,6 +118,7 @@ predict.assam <- function(object,
     #' # Checks and balances
     ##-----------------------
     num_spp <- length(object$spp_intercepts)
+    num_units <- nrow(newdata)
     
     if(!inherits(object, "assam")) 
         stop("`object' is not of class \"assam\"")
@@ -149,32 +148,35 @@ predict.assam <- function(object,
     ##-----------------------
     #' # Construct X and associated point predictions
     ##-----------------------
-    tmp_formula <- as.formula(paste("response", paste(as.character(object$formula),collapse = " ") ) )
-    nullfit <- sdmTMB(tmp_formula, 
-                      spatial = FALSE,
-                      data = data.frame(newdata, response = rnorm(nrow(newdata)))) #' This may not work in the future if smootihng terms are included, say, due to the standardization that needs to be applied    
-    X <- model.matrix(nullfit$formula[[1]], data = nullfit$data)[,-1] # Remove the intercept term
-    num_units <- nrow(X)
-    
-    get_eta <- tcrossprod(X, object$betas)
-    if(!is.null(newoffset))
-        get_eta <- get_eta + newoffset
-
-    
     if(type == "archetype") {
+        tmp_formula <- as.formula(paste("response", paste(as.character(object$formula),collapse = " ") ) )
+        nullfit <- sdmTMB(tmp_formula, 
+                          spatial = FALSE,
+                          data = data.frame(newdata, response = rnorm(nrow(newdata)))) #' This may not work in the future if smootihng terms are included, say, due to the standardization that needs to be applied    
+        X <- model.matrix(nullfit$formula[[1]], data = nullfit$data)[,-1] # Remove the intercept term
+        
+        get_eta <- tcrossprod(X, object$betas)
+        if(!is.null(newoffset))
+            get_eta <- get_eta + newoffset
+        
         pt_pred <- get_eta 
         rownames(pt_pred) <- rownames(newdata)
         colnames(pt_pred) <- names(object$mixture_proportion)
+        rm(tmp_formula, nullfit)
         }
     
     if(type %in% c("species_max", "species_mean")) {
         all_spp_mu <- array(NA, dim = c(num_units, num_spp, object$num_archetypes),
-                                 dimnames = list(units = rownames(newdata), spp = names(object$spp_intercepts), archetype = names(object$mixture_proportion)))
+                            dimnames = list(units = rownames(newdata), spp = names(object$spp_intercepts), archetype = names(object$mixture_proportion)))
+        
+        #' Note this is not parallelized since uncertainty quantification later on is!
         for(k0 in 1:object$num_archetypes) {
-            all_spp_mu[,,k0] <- matrix(object$spp_intercepts, nrow = num_units, ncol = num_spp, byrow = TRUE) + matrix(get_eta[,k0], nrow = num_units, ncol = num_spp, byrow = FALSE)
+            all_spp_mu[,,k0] <- .predict_eta_sdmTMB(object = object, newdata = newdata, k0 = k0)
+            
             if(!is.null(newoffset))
                 all_spp_mu[,,k0] <- all_spp_mu[,,k0] + newoffset
-            }    
+            }
+            
         all_spp_mu <- object$family$linkinv(all_spp_mu)
         
         if(type == "species_max") {
@@ -204,11 +206,11 @@ predict.assam <- function(object,
             cw_betas <- cw_bootstrap_parameters[grep("beta[1-9]", names(cw_bootstrap_parameters))]             
             cw_betas <- matrix(cw_betas, nrow = object$num_archetypes, byrow = TRUE)
 
-            get_cw_eta <- tcrossprod(X, cw_betas)
-            if(!is.null(newoffset))
-                get_eta <- get_eta + newoffset
-            
             if(type == "archetype") {
+                get_cw_eta <- tcrossprod(X, cw_betas)
+                if(!is.null(newoffset))
+                    get_eta <- get_eta + newoffset
+                
                 pt_pred <- get_cw_eta 
                 rownames(pt_pred) <- rownames(newdata)
                 colnames(pt_pred) <- names(object$mixture_proportion)
@@ -264,5 +266,73 @@ predict.assam <- function(object,
     }
 
 
- 
+
+# Hidden function to predict species-specific predictions for
+# The sdmTMB object is extracted and then manipulated into a new TMB object fixed at the asSAM parameter estimates. This is then used as the basis for prediction
+#' @noMd
+#' @noRd
+.predict_eta_sdmTMB <- function(object, newdata, k0) {
+    out <- NULL    
+    
+    for(l1 in 1:length(object$spp_intercepts)) {
+        #' Set up new parameters as per the asSAM
+        use_pars <- .get_pars2(object = object$sdmTMB_fits[[l1]])
+        use_pars[["b_j"]] <- c(object$spp_intercepts[l1], object$betas[k0,])
+        if(object$family$family[1] %in% c("Beta", "gaussian", "Gamma", "nbinom2", "tweedie")) 
+            use_pars[["ln_phi"]] <- log(object$spp_nuisance$dispersion[l1])
+        if(object$family$family[1] == "tweedie") 
+            use_pars[["thetaf"]] <- qlogis(object$spp_nuisance$power[l1] - 1)
+        if(!is.null(object$mesh)) {
+            use_pars[["ln_kappa"]] <- matrix(log(1/object$spp_nuisance$spatial_range[l1]), nrow = 2, ncol = 1) # This has two rows as set up as sdmTMB
+            use_pars[["ln_tau_O"]] <- 1/(sqrt(4*pi) * object$spp_nuisance$spatial_SD[l1] * exp(use_pars[["ln_kappa"]][1,1])) 
+            }
+        
+        #' Truncate spatial parameters that are very large in magnitude 
+        if(use_pars[["ln_tau_O"]] < -30) use_pars[["ln_tau_O"]] <- -30
+        if(use_pars[["ln_tau_O"]] > 30) use_pars[["ln_tau_O"]] <- 30
+        if(any(use_pars[["ln_kappa"]] < -30)) use_pars[["ln_kappa"]] <- matrix(-30, nrow = 2, ncol = 1)
+        if(any(use_pars[["ln_kappa"]] > 30)) use_pars[["ln_kappa"]] <- matrix(30, nrow = 2, ncol = 1)
+        
+        #' Set up new MAP to constrain all parameters at asSAM estimates
+        use_map <- object$sdmTMB_fits[[l1]]$tmb_map
+        use_map$b_j <- as.factor(rep(NA, length(use_pars[["b_j"]])))
+        if(object$family$family[1] %in% c("Beta", "gaussian", "Gamma", "nbinom2", "tweedie"))
+            use_map$ln_phi <- as.factor(NA)
+        if(object$family$family[1] == "tweedie") 
+            use_map$thetaf <- as.factor(NA)
+        if(!is.null(object$mesh)) {
+            use_map$ln_tau_O <- as.factor(NA)
+            use_map$ln_kappa <- as.factor(matrix(NA, 2, 1)) 
+            }
+        
+        #' Construct new TMB object
+        new_tmb_obj <- TMB::MakeADFun(data = predict(object$sdmTMB_fits[[l1]], newdata = newdata, return_tmb_object = TRUE)$pred_tmb_data,
+                                      profile = object$sdmTMB_fits[[l1]]$control$profile,
+                                      parameters = use_pars,
+                                      map = use_map,
+                                      random = object$sdmTMB_fits[[l1]]$tmb_random,
+                                      DLL = "sdmTMB",
+                                      silent = TRUE)
+        
+        new_tmb_obj$fn(new_tmb_obj$par) # need to initialize the new TMB object once
+        new_tmb_sdreport <- TMB::sdreport(new_tmb_obj, par.fixed = new_tmb_obj$par) # Update random effects
+        r <- new_tmb_obj$report(new_tmb_obj$env$last.par) # last.par taken since it is the newest set of parameters
+        
+        out <- cbind(out, r$proj_eta[,1])
+        }
+
+    return(out)
+    }
+    
+# Hidden function taken directly from sdmTMB utils.R
+#' @noMd
+#' @noRd
+.get_pars2 <- function(object) {
+    # based on glmmTMB:
+    ee <- object$tmb_obj$env
+    x <- ee$last.par.best
+    if (length(ee$random) > 0) x <- x[-ee$random]
+    p <- ee$parList(x = x)
+    p
+}
 
